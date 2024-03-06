2f4248582ffe4011461bc99b7a2f110c8cd6d9a6
Spaghettimonstar <130066887+Spaghettimonstar@users.noreply.github.com>
2024-03-06 16:35:37 +0000

    Started scaffolding

--- main.cs
+++ main.cs
@@ -1,9 +1,36 @@
 using System;
 class Program{
 public static void Main(string[] args)  {
-  Console.WriteLine("Hello World");
+  Console.WriteLine("Hello World"); 
+}
+}
+
+abstract class Game{
+
+//Function to reset variables after x is done
+// Display  screens(Start screen, Point shop, chimera creator, fight screen, game over screen )
   
 }
+class Player{
+// has a pointpool variablre
+//has a name variable
+
+  
+//has a method that reads inputs
+  
+}
+abstract class Attributes{
+// define general attributes (mobility,defense, etc)
+
+
+}
+class Modifiers{
+//variable to store pointvalue (neg or pos) for each part
+//
+  
+}
+class Chimera:Attributes {
+
 
   
 }
